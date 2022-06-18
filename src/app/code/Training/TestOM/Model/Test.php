9d9336d3b21253b8ce0257c3d28d318af8acf9ad
Eduard Lelikov <eduard.lelikov@onilab.com>
2022-06-18 18:48:33 +0300

    HW-1.5

--- src/app/code/Training/TestOM/Model/Test.php
+++ src/app/code/Training/TestOM/Model/Test.php
@@ -8,21 +8,25 @@ class Test
     private $_arrayList;
     private $_name;
     private $_number;
+    private $_managerFactory;
 
     public function __construct(
         ManagerInterface $manager,
         $name,
         int $number,
-        array $arrayList
+        array $arrayList,
+        \Training\TestOM\Model\ManagerInterfaceFactory $managerFactory
     ) {
         $this->_manager = $manager;
         $this->_name = $name;
         $this->_number = $number;
         $this->_arrayList = $arrayList;
+        $this->_managerFactory = $managerFactory;
     }
 
     public function log(): string
     {
+        $newManager = $this->_managerFactory->create();
         return '<pre>'
             . print_r(get_class($this->_manager), true)
             . '<br>'
@@ -31,6 +35,8 @@ class Test
             . print_r($this->_number, true)
             . '<br>'
             . print_r($this->_arrayList, true)
+            . '<br>'
+            . print_r($newManager, true)
             . '</pre>';
     }
 }
