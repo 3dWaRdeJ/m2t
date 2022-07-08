73abc66673d2dc2fd878ede3556f37026de4a787
Eduard Lelikov <eduard.lelikov@onilab.com>
2022-07-08 23:27:33 +0300

    HW-3.3

--- src/app/code/Training/Test/Controller/Block/Index.php
+++ src/app/code/Training/Test/Controller/Block/Index.php
@@ -10,21 +10,28 @@ class Index extends \Magento\Framework\App\Action\Action
      * @var \Magento\Framework\View\LayoutFactory
      */
     private $layoutFactory;
+    /** @var \Magento\Framework\Controller\Result\RawFactory $rawFactory */
+    private $rawFactory;
+
     /**
      * @param \Magento\Backend\App\Action\Context $context
      * @param \Magento\Framework\View\LayoutFactory $layoutFactory
      */
     public function __construct(
         \Magento\Backend\App\Action\Context $context,
-        \Magento\Framework\View\LayoutFactory $layoutFactory
+        \Magento\Framework\View\LayoutFactory $layoutFactory,
+        \Magento\Framework\Controller\Result\RawFactory $rawFactory
     ) {
         $this->layoutFactory = $layoutFactory;
+        $this->rawFactory = $rawFactory;
         parent::__construct($context);
     }
     public function execute()
     {
+        $result = $this->rawFactory->create();
         $layout = $this->layoutFactory->create();
         $block = $layout->createBlock(\Training\Test\Block\Test::class);
-        $this->getResponse()->appendBody($block->toHtml());
+        $result->setContents($block->toHtml());
+        return $result;
     }
 }
