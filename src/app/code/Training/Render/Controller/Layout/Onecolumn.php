4a85d9846be6ae5dc71ef0fa14031a8977d5baa1
Eduard Lelikov <eduard.lelikov@onilab.com>
2022-07-11 06:41:55 +0300

    HW-3.10

---
+++ src/app/code/Training/Render/Controller/Layout/Onecolumn.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Training\Render\Controller\Layout;
+
+class Onecolumn extends \Magento\Framework\App\Action\Action
+{
+    private $pageFactory;
+
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $pageFactory)
+    {
+        $this->pageFactory = $pageFactory;
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        return $this->pageFactory->create();
+    }
+}
